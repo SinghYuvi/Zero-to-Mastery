ce12502e4bde52fcdf4d86d165ec88c7612784b0
Yuvraj Singh <singhyuvraj1995@yahoo.com>
2023-04-17 10:02:10 +0530

    16 Apr changes

---
+++ JavaScript/exercise1.js
@@ -0,0 +1,38 @@
+//Evaluate the below:
+var test = 5 + "34";
+var test1 = 5 - "4";
+var test2 = 10 % 5;
+var test3 = 5 % 10;
+var test4 = "Java" + "Script";
+var test5 = " " + " ";
+var test6 = " " + 0 ;
+var test7 = true + true;
+var test8 = true + false;
+var test9 = false + true;
+var test10 = false - true;
+var test11 = 3 - 4;
+var test12 = "Bob" - "bill";
+
+console.log(`Evaluation is :${test12}`);       
+//Evaluate the below comparisons:
+var test13 = 5 >= 1;
+var test14 = 0 === 1;
+var test15 = 4 <= 1;  
+var test16 = 1 != 1; 
+var test17 = "A" > "B";               
+var test18 = "B" < "C";               
+var test19 = "a" > "A";               
+var test20 = "b" < "A";              
+var test21 = true === false; 
+var test22 = true != true; 
+
+console.log(`Comparision is :${test22}`);       
+ 
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+var text = "Hi There!"+" "+"It\'s \"sunny\" out";
+console.log(`${text}`);
+ 
+ 
+
+
+ 
---
+++ JavaScript/exercise2.js
@@ -0,0 +1,15 @@
+var firstName = "Yuvraj";
+var lastName = "Singh";
+
+var fullName = firstName + lastName;
+console.log(`Your Full Name is : ${firstName} ${lastName}`);
+
+
+var a = 34;
+var b = 21;
+var c;
+
+a = 2;
+var sum = a + b;
+console.log(`Sum of a & b is : ${sum}`);
+console.log(`c is : ${c}`);
