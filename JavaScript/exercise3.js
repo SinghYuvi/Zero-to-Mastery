f58969dacd36f963f3d7e67eaa3456b73322b046
Yuvraj Singh <singhyuvraj1995@yahoo.com>
2023-04-23 21:26:51 +0530

    23 Apr Commit

---
+++ JavaScript/exercise3.js
@@ -0,0 +1,12 @@
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.  
+
+/////Below code can be run in browser console as prompt() is an Windows function. Alert() is also an alternative of prompt()
+firstNumber = prompt("Enter first number");
+secondNumber = prompt("Enter second number");
+
+var sum = firstNumber + secondNumber;
+prompt(`Sum is: ${sum}`);
